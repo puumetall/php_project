993dc41c9c058799eed271034c83bff104f5aefd
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2022-09-02 15:43:01 +0300

    posts example

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
-$content = file_get_contents('list.txt');
-$items = explode("\n",$content);
-array_pop($items);
+$json = file_get_contents('posts.json');
+$posts = json_decode($json, true);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,19 +8,29 @@ array_pop($items);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     <title>Document</title>
 </head>
 <body>
-    <form action="/add.php" method="POST">
-        <input type="text" name="newitem">
-        <input type="submit" value="Add">
-    </form>
-    <ul>
-        <?php foreach($items as $item): ?>
-            <li>
-                <?=$item?>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <div class="container">
+        <a class="button is-primary" href="/new.php">New Post</a>
+            <?php foreach($posts as $post): ?>
+                <div class="card">
+                    <header class="card-header">
+                        <p class="card-header-title">
+                            <?=$post['title']?>
+                        </p>
+                    </header>
+                    <div class="card-content">
+                        <div class="content">
+                            <?=$post['body']?>
+                        </div>
+                    </div>
+                    <footer class="card-footer">
+                        <a href="#" class="card-footer-item">Read more</a>
+                    </footer>
+                </div>
+            <?php endforeach; ?>
+    </div>
 </body>
 </html>
